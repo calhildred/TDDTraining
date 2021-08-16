50c78f0f9d0eae1b8c8044c3a552c25f966f34f2
Callum Hildred <callumhildred@gmail.com>
2021-08-16 22:39:07 +1200

    First commit

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using TDDTraining.domain;
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
